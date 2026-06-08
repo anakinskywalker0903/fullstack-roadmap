015ac026f438422057884b0d19511d7dd2d0b4ef
Rohit Dubey <rohitdubey39005@gmail.com>
2026-06-08 20:24:27 +0530

    Objects in depth done

--- js/02_basics/03_objects.js
+++ js/02_basics/03_objects.js
@@ -14,12 +14,22 @@ const JsUser = {
     isLoggedIn : false,
     lastLoginDays: ["Monday","Saturday"]
 }
-console.log(JsUser.email)
-console.log(JsUser["email"])
-console.log(JsUser["fullname"])
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["fullname"])
 // console.log(JsUser[mySym])
 
 JsUser.email = "rohit@chagpt.com"
 Object.freeze(JsUser)
 JsUser.email = "rohit@microsoft.com"
-console.log(JsUser);
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello Js user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello Js user,${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
