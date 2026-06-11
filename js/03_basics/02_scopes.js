8df609b6953a1edfcdaf29203de50feb824b5a2f
Rohit Dubey <rohitdubey39005@gmail.com>
2026-06-11 11:45:53 +0530

    JS Scopes Done

--- js/03_basics/02_scopes.js
+++ js/03_basics/02_scopes.js
@@ -1,19 +1,59 @@
+
 // var c = 300
 let a = 300
 
 if(true){
     let a = 10
     const b = 20
-    console.log("INNER: ",a);
-    
-
+    // console.log("INNER: ",a);
 }
 
-// for (let index = 0; index < array.length; index++) {
-//     const element = array[index];
-// }
 
-
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one() {
+    const username = "Rohit"
+
+    function two() {
+        const website ="youtube"
+        console.log(username);
+    }
+    // console.log(website);
+    
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "Rohit"
+    if (username === "Rohit ") {
+        const website = " youtube"     
+        console.log(username  +  website);
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+
+//+++++++++++++ Interesting +++++++++++++++++++
+
+
+console.log(addone(5))
+
+function addone(num) {
+    return num + 1    
+}
+
+
+
+addTwo(5)
+const addTwo = function(num){
+        return num + 2
+}
+
