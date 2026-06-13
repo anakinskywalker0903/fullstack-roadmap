96b446a8afcf2e935bc59399a503e98abe3a97b3
Rohit Dubey <rohitdubey39005@gmail.com>
2026-06-13 12:12:04 +0530

    Control Flow Js Done

---
+++ js/04_control_flow/one.js
@@ -0,0 +1,53 @@
+// IF
+const isUserloggedIn = true
+const temperature = 41
+
+// if (temperature===40) {
+//     console.log("less than 50");
+// }else {
+//         console.log("Temperature is greater than 50");
+// }
+
+// console.log("excute");
+// <,>,<=,>=,==,!=,===,!==
+
+// const score = 200
+
+// if(score>100){
+//     let power ="fly"
+//     console.log(`User Power ${power}`);
+// }
+
+// console.log(`User Power ${power}`)
+
+
+const bal = 1000
+
+// if (bal>500) console.log("test"),console.log("test2");
+
+// if (bal<500) {
+//     console.log("less than 500");
+// } else if (bal<750){
+
+//     console.log("less than 750"); 
+// } else if (bal<900) {
+
+//     console.log("less than 900");
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userloggedin = true
+const debitcard = true
+const loggedinfromgoogle = false
+const loggedinfromemail = true
+
+if (userloggedin && debitcard) {
+    console.log("Allow to buy ")
+}
+
+if (loggedinfromgoogle || loggedinfromemail) {
+    console.log("User Logged in");
+}
+
