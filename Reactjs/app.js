23397af23ec68cc7cecb510dfe8395f2fc851886
Rohit Dubey <rohitdubey39005@gmail.com>
2026-06-25 16:46:47 +0530

    ReactJS Started

---
+++ Reactjs/app.js
@@ -0,0 +1,7 @@
+// export const arr = [10,20,30]
+
+const user = 'Rohit'
+export const age = 89 
+export const skills = ['Frontend','Backend','UI/UX']
+
+export default user 
---
+++ Reactjs/script.js
@@ -0,0 +1,23 @@
+import username from './app.js'
+import {age}  from './app.js'
+import {skills}  from './app.js'
+import user2 from './test.js';
+// var h1 = document.createElement('h1')
+
+// h1.innerHTML = 'Hello from JS'
+
+// document.body.appendChild(h1)
+
+
+// Library 
+//     -GSAP
+//     -Lenis
+//     -ReactJS
+
+// Framework 
+//     -NextJS
+//     -Angular
+
+
+
+console.log(username,user2);
