c3b4d6264106dec69b7fbb574a2657a1c7fd3057
Rohit Dubey <rohitdubey39005@gmail.com>
2026-06-26 19:36:00 +0530

    props done

--- Reactjs/03-props/src/App.jsx
+++ Reactjs/03-props/src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   return (
     <div className='parent'>
       <Card user='Aman' age={18}/>
+      <Card user='Sarthak' age={21}/>
     </div>
   )
 }
--- Reactjs/03-props/src/components/Card.jsx
+++ Reactjs/03-props/src/components/Card.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
 function Card(props) {
+
+    console.log(props);
+
+
   return (
     <div className="card">
         <img src="https://images.unsplash.com/photo-1704748429943-12b2d04092e5?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDQ4fENEd3V3WEpBYkV3fHxlbnwwfHx8fHw%3D" alt="" />
-            <h1>Rohit Dubey</h1>
+            <h1>{props.user}</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et, quidem?</p>
             <button>View Profile</button>
           </div>
