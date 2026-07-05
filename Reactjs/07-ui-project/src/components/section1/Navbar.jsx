5b24354f88fdfeae1ad1abef5289923d7867a563
Rohit Dubey <rohitdubey39005@gmail.com>
2026-07-05 16:01:55 +0530

    UI - Project & Functions

--- Reactjs/07-ui-project/src/components/section1/Navbar.jsx
+++ Reactjs/07-ui-project/src/components/section1/Navbar.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between py-4 px-10'>
-        <h4 className='bg-black text-white px-5 py-3 uppercase rounded-full text-sm'>Target Audience </h4>
-        <button className='bg-gray-400 rounded-full px-4 py-2 uppercase tracking-wider text-sm'>Digital Banking PLatform</button>
+    <div className='flex items-center justify-between py-8 px-18'>
+        <h4 className='bg-black text-white uppercase px-6 py-2 rounded-full'>Target Audience</h4>
+        <button className='bg-gray-200 px-6 py-2 uppercase rounded-full tracking-widest text-sm'>digital Banking Platform</button>
     </div>
   )
 }
---
+++ Reactjs/08-functions/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const App = () => {
+
+
+  function inputChanging(){
+    console.log('User is Typing');
+    
+  }
+
+  return (
+    <div>
+      <input onChange={function (elem) {
+        console.log(elem.target.value);
+      }} 
+      type="text" 
+      placeholder='Enter Name' />
+    </div>
+  )
+}
+
+export default App
