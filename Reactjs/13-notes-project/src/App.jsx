f650b3c711301f5bbaa115e22cc0d2bd8ef7caff
Rohit Dubey <rohitdubey39005@gmail.com>
2026-07-09 15:32:50 +0530

    uncomplete notes project

---
+++ Reactjs/13-notes-project/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div>
+      <form className='flex justify-between bg' action="">
+          <input 
+          
+          type="text" 
+          placeholder='Enter Task Heading' 
+          />
+          <input 
+          type="text" 
+          className='' 
+          />
+      </form>
+    </div>
+  )
+}
+
+export default App
